b5f2a50bc4f28e8b750e4e1e47634f622de1d970
xcplanet <brianlsullivan@fas.harvard.edu>
2015-09-02 21:23:54 -0400

    fixed non-functional logic better

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
-var num = 0;
-
-var oneUp = function(){
-  num++;
-  printNum();
-};
-
-var printNum = function(){
-  $('#output').html(num);
-};
-
 $(function(){
+  var num = 0;
+
+  var oneUp = function(){
+    num++;
+    printNum();
+  };
+
+  var printNum = function(){
+    $('#output').html(num);
+  };
+
+  $("#my-button" ).click(function() {
+    oneUp();
+    printNum();
+  });
+
   printNum();
 });
