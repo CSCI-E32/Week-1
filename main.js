9d19be3830e2ce1c1e65c6d1ab477e7d89394ae6
Yang Chen <yang1983chen@gmail.com>
2015-09-02 20:39:14 -0400

    fixed a bug

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 $(function(){
+  var num = 0;
+
   var oneUp = function(){
     num++;
   };
+
   var printNum = function(){
-    var num = 0;
     $('#output').html(num);
   };
+
   printNum();
+
+  $('#my-button').click(function() {
+    oneUp();
+    printNum();
+  });
 });
