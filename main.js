422eb57b5daa2cee4d5cd3b078368849a9dbc4d5
Aaron Marden <aaron.marden@gmail.com>
2015-09-02 20:21:11 -0400

    fix bug

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 $(function(){
-  var oneUp = function(){
+var num = 0;  
+var oneUp = function(){
     num++;
   };
   var printNum = function(){
-    var num = 0;
     $('#output').html(num);
   };
   printNum();
+  $('#my-button').on('click', function() { oneUp(); printNum(); });
 });
