6fd7fd650e6a7cd8a48819f7f247c17c945bf48c
gengi <gianluca.costante@gmail.com>
2015-09-02 20:37:38 -0400

    bug fixed

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 $(function(){
-  var oneUp = function(){
-    num++;
-  };
-  var printNum = function(){
-    var num = 0;
-    $('#output').html(num);
-  };
-  printNum();
+
+	var num = 0;
+
+  	var oneUp = function(){
+     	num++;
+    	printNum();
+  	};
+  
+  	var printNum = function(){
+    	$('#output').html(num);
+  	};
+
+  	$('#my-button').click(oneUp);
+  	printNum();
 });
