611fcc3e5f935b1c9b9bcc2d4c78d961bbe9d400
lmpeterman <lauramay08@gmail.com>
2015-09-03 07:05:38 -0400

    Figured it out!

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
+// Used www.w3schools.com jQuery reference for some review.
 $(function(){
+  var num = 0;
   var oneUp = function(){
     num++;
   };
   var printNum = function(){
-    var num = 0;
     $('#output').html(num);
   };
   printNum();
+  $('#my-button').click(function(){
+    oneUp();
+    printNum();
+  });
 });
