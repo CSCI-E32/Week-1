7db5e003ddf718a9ff640ef9a4337cbf2d09581b
Adam Beerman <adam.beerman@gmail.com>
2015-09-02 21:02:20 -0400

    main.js bug fix

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 $(function(){
-  var oneUp = function(){
+  var num = 0;
+var oneUp = function(){
     num++;
+    printNum();
   };
   var printNum = function(){
-    var num = 0;
     $('#output').html(num);
   };
   printNum();
+
+  $("#my-button").click(oneUp);
 });
