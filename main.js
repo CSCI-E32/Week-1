d3b74ac736d20555f591378baf890ae629079631
Drew Teter <mteter13@gmail.com>
2015-09-02 20:06:38 -0400

    Hoisted the num variable so that it's accessible by the oneUp function. oneUp now calls printNum. Added an event listener.

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 $(function(){
+  var num = 0;
   var oneUp = function(){
     num++;
+    printNum();
   };
   var printNum = function(){
-    var num = 0;
     $('#output').html(num);
   };
   printNum();
+  $('#my-button').click(oneUp);
 });
